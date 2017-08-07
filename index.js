c9cdbf5c7ddb47b29e4f19117b7adaebb325a039
Oladipupo Olimene <dipotope@yahoo.com>
2017-08-07 13:49:49 +0100

    error handler

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 var express 			= require("express"),
 	app					= express(),
+	appStoreReviews 	= require("./reviews-to-console");
 	
+
+// serve static files
+	app.use(express.static(__dirname + "/www"));
+
+// utilizing the body-parser to accept json file 
+	//app.use(bps.json());
+
+// mount routes
+	//app.use("/", appStoreReviews);
+
+// Your error handler or application error handler must be the last endpoint in your list of routes 
+	app.use((err, req, res, next) => {
+		res.status(422).send({ error: err.message });
+
+	});
+
